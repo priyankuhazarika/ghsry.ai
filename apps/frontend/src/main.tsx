6ec829d926f21523a24624ca190c8f2ab26d43f2
Snehanshu Phukon <hello@snehanshu.com>
2024-02-19 02:52:35 +0530

    integrate with fusionauth

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { FusionAuthProvider } from "@fusionauth/react-sdk";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <FusionAuthProvider
+      clientID="28e3d7bd-58df-4d95-9046-525866219ba3"
+      serverUrl="http://localhost:9011"
+      redirectUri="http://localhost:6080/fa-login-redirect"
+    >
+      <App />
+    </FusionAuthProvider>
   </React.StrictMode>,
 );
